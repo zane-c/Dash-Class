a7c740ed0eb29177082f192b33c7338d334b388b
zane-c <zanedougall@gmail.com>
2017-10-08 01:24:22 -0700

    Working poll submission!

--- src/components/src/Polls.jsx
+++ src/components/src/Polls.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { RadioGroup, ReversedRadioButton } from 'react-radio-buttons';
+import { updatePolls } from '../../actions/dashboard.js';
 import styles from './Polls.scss';
 
 class Polls extends React.Component {
@@ -7,53 +10,71 @@ class Polls extends React.Component {
     super(props);
     this.state = {
       submitted: false,
-      answer: 'Merge sort', // set to first item in options array
-      options: [
-        { id: 1, text: 'Merge sort' },
-        { id: 2, text: 'Quick sort' },
-        { id: 3, text: 'Pogo sort' },
-      ],
+      answer: null,
     };
     this.onChange = answer => this.setState({ answer });
     this.onSubmit = this.onSubmit.bind(this);
   }
-  onSubmit() {
+  onSubmit(defaultOption) {
+    const { pollData } = this.props;
+    const answer = this.state.answer || defaultOption;
+
+    const newData = pollData.map((poll) => {
+      const copy = poll;
+      if (poll.state !== 'inactive') {
+        copy.options = poll.options.map((opt) => {
+          const copy2 = opt;
+          if (copy2.text === answer) {
+            copy2.votes += 1;
+          }
+          return copy2;
+        });
+      }
+      return copy;
+    });
+
     this.setState({ submitted: true });
-    // save answer to database
+    this.props.updatePollsAction(newData);
   }
   render() {
-    const { answer, options, submitted } = this.state;
+    const { answer, submitted } = this.state;
+    const { pollData } = this.props;
+    const activePoll = pollData.filter(poll => (poll.state !== 'inactive'))[0];
+
     return (
       <div className={styles.container}>
         <div className={styles.title}>
           Polls
         </div>
         <div className={styles.content}>
-          {options.length === 0 ?
+          {!activePoll ?
             <div className={styles.pollText}>
               No active polls
             </div>
             :
             <div>
               <div className={styles.pollText}>
-                What is your favorite sorting algorithm?
+                {activePoll.text}
               </div>
               {submitted ?
                 <div className={styles.answer}>
-                  You picked {answer}
+                  You picked {answer || activePoll.options[0].text}
                 </div>
                 :
                 <div className={styles.options}>
                   <RadioGroup onChange={this.onChange}>
-                    {options.map(opt => (
-                      <ReversedRadioButton key={opt.id} value={opt.text}>
+                    {activePoll.options.map((opt, idx) => (
+                      <ReversedRadioButton
+                        key={`${opt.text}${idx}`}
+                        value={opt.text}
+                      >
                         {opt.text}
                       </ReversedRadioButton>
                     ))}
                   </RadioGroup>
                   <div
                     className={styles.button}
-                    onClick={this.onSubmit}
+                    onClick={() => this.onSubmit(activePoll.options[0].text)}
                   >
                     Submit
                   </div>
@@ -67,4 +88,17 @@ class Polls extends React.Component {
   }
 }
 
-export default Polls;
+Polls.propTypes = {
+  pollData: PropTypes.array.isRequired,
+  updatePollsAction: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  pollData: state.dashboard.pollData,
+});
+
+const mapDispatchToProps = dispatch => ({
+  updatePollsAction: data => dispatch(updatePolls(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Polls);
